79b27b49a1519d1e0fda01641cb6ead3523a46f5
Nico Zessoules <nicozessoules@gmail.com>
2025-12-11 23:43:45 -0700

    init tailwind

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,8 +5,14 @@ import qwikdev from '@qwikdev/astro';
 
 import vercel from '@astrojs/vercel';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [qwikdev()],
-  adapter: vercel()
+  adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
