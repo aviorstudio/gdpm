bc92449f70b7683886dae5f06c62cddd572b25f5
Nico Zessoules <nicozessoules@gmail.com>
2025-12-13 17:36:24 -0700

    rename /username to /@username

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -9,6 +9,20 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/[username]': {
+      status: 301,
+      destination: '/@[username]',
+    },
+    '/[username]/[pkg]': {
+      status: 301,
+      destination: '/@[username]/[pkg]',
+    },
+    '/[username]/account': {
+      status: 301,
+      destination: '/@[username]/account',
+    },
+  },
   integrations: [qwikdev()],
   adapter: vercel(),
 
