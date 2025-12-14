c7217bf95ecccacfc7527e35f2cf75870fe81947
Nico Zessoules <nicozessoules@gmail.com>
2025-12-13 22:55:33 -0700

    remove bad redirects

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -9,20 +9,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  redirects: {
-    '/[username]': {
-      status: 301,
-      destination: '/@[username]',
-    },
-    '/[username]/[plugin]': {
-      status: 301,
-      destination: '/@[username]/[plugin]',
-    },
-    '/[username]/account': {
-      status: 301,
-      destination: '/@[username]/account',
-    },
-  },
   integrations: [qwikdev()],
   adapter: vercel(),
 
